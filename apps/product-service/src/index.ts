aa3031bd5080f1f71798c2c48b07c3472065b043
Uygar Muntas <muntasuygar@gmail.com>
2026-06-19 01:06:36 +0300

    added product-db package with prisma schema and product service controllers and routes

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,13 +1,17 @@
-import express , {Request,Response} from "express"
+import express , {NextFunction, Request,Response} from "express"
 import cors from "cors"
 import {clerkMiddleware, getAuth} from '@clerk/express'
 import { shouldBeUser } from "./middleware/authMiddleware.js"
+import productRouter from "./routes/product.route"
+import categoryRouter from "./routes/category.route"
+
 
 const app = express()
 app.use(cors({
     origin:["http://localhost:3002","http://localhost:3003" ],
     credentials:true
 }))
+app.use(express.json())
 app.use(clerkMiddleware())
 
 app.get("/health", (req:Request, res:Response)=>{
@@ -22,6 +26,14 @@ app.get("/test",shouldBeUser, (req,res)=> {
     
     res.json({message:"Product service authenticated", userId:req.userId})
 
+})
+
+app.use("/products", productRouter)
+app.use("/category", categoryRouter)
+
+app.use((err:any, req:Request, res:Response, next:NextFunction)=> {
+    console.log(err)
+    return res.status(err.status || 500).json({message:err.message || "Inter server error!"})
 })
 
 app.listen(8000, ()=>{
